53b42c3c82437d068514d7c6ae1496a78ecb142b
AdityaArnav <99977930+AdityaArnav@users.noreply.github.com>
2022-04-02 21:43:58 +0530

    final submission by me(Aditya Arnav)

---
+++ AdityaArnav/credit/credit.js
@@ -0,0 +1,76 @@
+// let tableBody = document.getElementById("tbody");
+// table()
+// function table(){
+
+//     let row1 = document.createElement("tr")
+ 
+//     let td1 = document.createElement("td");
+//     td1.innerText = "Earn 10% back in Reward Dollars,* or receive 6 months special financing on purchases of $749 or more** at CB2"
+
+//     let r1=document.createElement('td')
+//     r1.innerText=">"
+
+//     let r2 =document.createElement('td')
+//     r2.innerText=">"
+
+//     row1.append(td1,r1,r2);
+
+
+
+//     let row2 = document.createElement("tr")
+
+//     let col2=document.createElement('td')
+//     col2.innerText="Earn 2% back in Reward Dollars on grocery store purchases*"
+
+//     let r5=document.createElement('td')
+//     r5.innerText=">"
+
+//     let r6 =document.createElement('td')
+//     r6.innerText=">"
+
+//     row2.append(col2,r5,r6);
+
+
+
+//     let row3 = document.createElement("tr")
+
+//     let col3=document.createElement("td")
+//     td1.innerText="Earn 1% back in Reward Dollars on every other purchase anywhere Mastercard is accepted*"
+
+//  let r7=document.createElement('td')
+//     r7.innerText=">"
+
+//     let r8 =document.createElement('td')
+//     r8.innerText=">"
+
+//   row3.append(col3,r7,r8)
+
+
+
+//   let row4 = document.createElement("tr")
+
+//     let col4=document.createElement('td')
+//     col4.innerText="No annual fee†"
+//     let r9=document.createElement('td')
+//     r9.innerText=">"
+
+//     let r0 =document.createElement('td')
+//     r0.innerText=">"
+
+// row4.append(col4,r9,r0)
+
+// let row5 = document.createElement("tr")
+
+//     let col5=document.createElement('td')
+//     col4.innerText="No annual fee†"
+//     let a1=document.createElement('td')
+//     r9.innerText=">"
+
+//     let a2 =document.createElement('td')
+//     r0.innerText=">"
+
+// row4.append(col5,a1,a2)
+    
+//      tableBody.append(row1,row2,row3,row4);
+
+// }
