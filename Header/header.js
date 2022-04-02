1685c89f7ca95e8510273e5b1efdea30a82bf3f3
Ajit Singh <79194669+ajit199@users.noreply.github.com>
2022-04-02 23:12:35 +0530

    created navbar and sidebar

---
+++ Header/header.js
@@ -0,0 +1,134 @@
+const navbar = () => {
+    return `  <div id="navbar">
+  
+    <div id="pappu">
+  
+      <div>
+        <a href="">UP TO 20% 0FF OUTDOOR</a>
+  
+        <span style="padding: 0 8px 0 8px;">|</span>
+  
+        <a href="">NEW TO SALE: UP TO 60% OFF</a>
+  
+        <span style="padding: 0 8px 0 8px;">|</span>
+  
+        <a href="">BUY NOW. PAY OVER TIME.</a>
+  
+        <span style="padding: 0 8px 0 8px;">|</span>
+  
+        <a href="">FREE DESIGN SERVICES</a>
+  
+        <span style="padding: 0 8px 0 8px;">|</span>
+  
+        <a href="">SEE ALL OFFERS (6)</a>
+      </div>
+  
+    </div>
+  
+  
+    <div id="rg">
+  
+      <div>
+  
+        <div id="sidebar">
+          <span>
+            <i id="bar" class="fa fa-bars fa-2x fa-fw" aria-hidden="false"></i>
+          </span>
+        </div>
+        <div id="hola">
+          <div id="input-div">
+            <div>
+              <button id="btn"><i class="fa-solid fa-magnifying-glass fa-2x"></i></button>
+              <input type="text" id="input-box">
+            </div>
+            <span id="line"></span>
+          </div>
+        </div>
+  
+      </div>
+      <div>
+        <a href=""><img
+            src="https://images.squarespace-cdn.com/content/v1/6059ecd2c49f5d3d9ef9a9ed/1617306626011-1C6XZRUS8I3N1HXILPWI/CB2+LOGO.png"
+            alt="Encrypted" id="ac"></a>
+      </div>
+      <div id="bicchu">
+  
+        <div class="dropdown">
+          <button class="dropbtn"><a href="">
+              <h2><i style="color:black" class="fa-solid fa-location-dot"></i></h2>
+            </a></button>
+          <div class="dropdown-content">
+            <a href="#"></a>
+  
+          </div>
+        </div>
+  
+        <div class="dropdown">
+          <button class="dropbtn"><a href="">
+              <h2><i style="color:black" class="fa-solid fa-user"></i></h2>
+            </a></button>
+          <div class="dropdown-content">
+            <a href="#">SIGN IN</a>
+            <a href="#">ORDER TRACK</a>
+            <a href="#">SCHEDULE DELIVERY</a>
+            <a href="#">CB2 CREDIT CARD</a>
+          </div>
+        </div>
+  
+        <div class="dropdown">
+          <button class="dropbtn"><a href="">
+              <h2><i style="color:black" class="fa-solid fa-heart"></i></h2>
+            </a></button>
+          <div class="dropdown-content">
+            <p>SOMETHING CATCH YOUR EYE?</p>
+  
+            <a href="#">Sign In</a>
+            <p>" To see items you maty have added using another computer or device."</p>
+  
+          </div>
+        </div>
+  
+        <div class="dropdown">
+          <button class="dropbtn"> <a href="">
+              <h2><i style="color:black" class="fa-solid fa-cart-shopping"></i></h2>
+            </a></button>
+          <div class="dropdown-content">
+  
+          </div>
+        </div>
+  
+      </div>
+  
+    </div>
+  
+  
+  </div>
+  <hr style="margin: 0;" />
+  
+    <div id="categories">
+      <ul type="none">
+        <li onclick="">SPRING '22 STORIES</li>
+        <li onclick="">NEW</li>
+        <li onclick="">FURNITURE</li>
+        <li onclick="">OUTDOOR</li>
+        <li onclick="">DECOR & MIRRORS</li>
+        <li onclick="">LIGHTING</li>
+        <li onclick="">PILLOWS & THROWS</li>
+        <li onclick="">RUGS</li>
+        <li onclick="">KITCHEN & DINING</li>
+        <li onclick="">BEDDING & BATH</li>
+        <li onclick="">GIFTS</li>
+        <li onclick="" style="color:red;">SALE & OFFERS</li>
+        <li onclick="">KRAVITZ DESIGN</li>
+        <hr style="width :8%; margin: 20px 0;border: 1px solid;">
+        <li onclick="">COLLABORATIONS</li>
+        <li onclick="">IDEAS & INSPIRATIONS</li>
+        <li onclick="">FREE DESIGN SERVICES</li>
+        <li onclick="">TRADE PROGRAM</li>
+      </ul>
+    </div>
+  `;
+  };
+  
+  export default navbar;
+  
